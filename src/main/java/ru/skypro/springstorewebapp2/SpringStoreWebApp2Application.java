83790a9799f17b7d52c171bc59f05f3ef62ff125
ad_ho <ad_hoc90@mail.ru>
2023-07-30 19:29:05 +0300

    initial commit

---
+++ src/main/java/ru/skypro/springstorewebapp2/SpringStoreWebApp2Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.springstorewebapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStoreWebApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStoreWebApp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/springstorewebapp2/SpringStoreWebApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.springstorewebapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStoreWebApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
